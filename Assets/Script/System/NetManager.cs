841eedbe130e88252363e4427ccb14008927bbcd
Daeno <thomas820605@gmail.com>
2013-11-30 23:42:13 +0800

    adding Network testing script

---
+++ Assets/Script/System/NetManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		PhotonNetwork.ConnectUsingSettings("0.1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnGUI(){
+		GUILayout.BeginArea (new Rect (10, 50, 100, 500));
+		GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+		foreach (Room game in PhotonNetwork.GetRoomList())
+		{
+			GUILayout.Label(game.name + " " + game.playerCount + "/" + game.maxPlayers);
+		}
+
+		GUILayout.EndArea();
+	}
+	void OnJoinedLobby()
+	{
+		PhotonNetwork.JoinRandomRoom();
+	}
+	void OnPhotonRandomJoinFailed()
+	{
+		PhotonNetwork.CreateRoom(null);
+	}
+	[RPC]
+	void SpawnEnemy(){
+		
+	}
+}
