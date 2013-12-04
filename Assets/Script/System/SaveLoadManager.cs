16e5042533989bbded4be661f11730def2c1787a
Tang Lee <andy135793726@gmail.com>
2013-12-04 20:25:26 +0800

    Sync before implementing SaveLoadManager

---
+++ Assets/Script/System/SaveLoadManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SaveLoadManager : MonoBehaviour {
+    public SystemMain systemMain;
+
+    public static string STAGE_STATE_STR = "stagestate";
+    
+    //stage
+    // "[passedstage0],[passedstage1],[ps2],...]/otherInformation1/otherInformation2"
+    //
+    //
+
+
+
+	// Use this for initialization
+	void Start () {
+        systemMain = GetComponent<SystemMain>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+
+    public void SaveSnapshot()
+    {
+        
+    }
+
+    public void SaveWhenEnd()
+    {
+    }
+
+    public void LoadSnapshot()
+    {
+       
+    }
+
+    public void LoadWhenBegin()
+    {
+    }
+
+
+}
